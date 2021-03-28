196b35494ef9cfad79153aa8a0826e64e8925a1d
honghainguyen777 <n.h.hai2015@gmail.com>
2021-03-28 15:30:09 +0200

    adding css and svg files - create custom modules for recognizing css and svg imported  files

--- src/components/Pizza.tsx
+++ src/components/Pizza.tsx
@@ -1,22 +1,25 @@
-import React from 'react';
+import React from "react";
+import PizzaCSS from "./Pizza.module.css";
 
 interface Pizza {
-    id: number;
-    name: string;
-    description: string;
-    price: number;
+  id: number;
+  name: string;
+  description: string;
+  price: number;
 }
 
 interface Props {
-    pizza: Pizza;
+  pizza: Pizza;
 }
 
 const Pizza: React.FC<Props> = ({ pizza }) => {
-    return <li>
-        <h2>{pizza.name}</h2>
-        <p>{pizza.description}</p>
-        <p>{pizza.price}</p>
+  return (
+    <li className={PizzaCSS.container}>
+      <h2>{pizza.name}</h2>
+      <p>{pizza.description}</p>
+      <p>{pizza.price}</p>
     </li>
+  );
 };
 
 export default Pizza;
--- webpack.config.js
+++ webpack.config.js
@@ -1,29 +1,43 @@
-const HtmlWebpackPlugin = require('html-webpack-plugin');
-const MiniCssExtractPlugin = require('mini-css-extract-plugin');
+const HtmlWebpackPlugin = require("html-webpack-plugin");
+const MiniCssExtractPlugin = require("mini-css-extract-plugin");
 
 module.exports = {
-  entry: './src/index.tsx',
+  entry: "./src/index.tsx",
   devtool: "eval-source-map",
   resolve: {
-    extensions: ['.js', '.ts', '.tsx'],
+    extensions: [".js", ".ts", ".tsx"],
   },
   module: {
     rules: [
       {
         test: /\.tsx?$/,
-        loader: 'babel-loader',
+        loader: "babel-loader",
         exclude: /node_modules/,
       },
       {
         test: /\.css/,
-        use: [MiniCssExtractPlugin.loader, { loader: 'css-loader' }],
-      }
+        use: [
+          MiniCssExtractPlugin.loader,
+          { loader: "css-loader", options: { modules: true } },
+        ],
+      },
+      {
+        test: /\.svg$/,
+        loader: "@svgr/webpack",
+        options: {
+          svgoConfig: {
+            plugins: {
+              removeViewBox: false,
+            }
+          }
+        }
+      },
     ],
   },
   plugins: [
     new HtmlWebpackPlugin({
-      template: './src/index.html',
-      filename: 'index.html',
+      template: "./src/index.html",
+      filename: "index.html",
     }),
     new MiniCssExtractPlugin(),
   ],
