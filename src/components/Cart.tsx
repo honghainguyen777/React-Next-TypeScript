544dbc344ba5cc03ea2eaeb0966ce899ef9d9fdc
honghainguyen777 <n.h.hai2015@gmail.com>
2021-03-28 21:55:08 +0200

    Using React context and useState hook

--- src/components/Cart.tsx
+++ src/components/Cart.tsx
@@ -1,6 +1,7 @@
 import React from "react";
 import { FiShoppingCart } from "react-icons/fi";
 import CartCSS from "./Cart.module.css";
+import { AppStateContext } from "./AppState";
 
 interface Props {}
 
@@ -24,32 +25,43 @@ class Cart extends React.Component<Props, State> {
 
   handleClick = (e: React.MouseEvent<HTMLButtonElement, MouseEvent>) => {
     if ((e.target as HTMLElement).nodeName === "SPAN") {
-      (e.target as HTMLSpanElement) // do more using .
+      e.target as HTMLSpanElement; // do more using .
     }
     this.setState((prevState) => ({ isOpen: !prevState.isOpen }));
-  }
+  };
 
   render() {
     return (
-      <div className={CartCSS.cartContainer}>
-        <button
-          className={CartCSS.button}
-          type="button"
-          onClick={this.handleClick}
-        >
-          <FiShoppingCart />
-          <span>2 pizza(s)</span>
-        </button>
-        <div
-          className={CartCSS.cartDropDown}
-          style={{ display: this.state.isOpen ? "block" : "none" }}
-        >
-          <ul>
-            <li>Pizza 1</li>
-            <li>Pizza 2</li>
-          </ul>
-        </div>
-      </div>
+      <AppStateContext.Consumer>
+        {(state) => {
+          return (
+            <div className={CartCSS.cartContainer}>
+              <button
+                className={CartCSS.button}
+                type="button"
+                onClick={this.handleClick}
+              >
+                <FiShoppingCart />
+                <span>{state.cart.items.length} pizza(s)</span>
+              </button>
+              <div
+                className={CartCSS.cartDropDown}
+                style={{ display: this.state.isOpen ? "block" : "none" }}
+              >
+                <ul>
+                  {state.cart.items.map((item) => {
+                    return (
+                      <li key={item.id}>
+                        {item.name} &times; {item.quantity}
+                      </li>
+                    );
+                  })}
+                </ul>
+              </div>
+            </div>
+          );
+        }}
+      </AppStateContext.Consumer>
     );
   }
 }
